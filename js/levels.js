585c4a27d52683347e2a4cfed154a5e5f9754755
Naveen Rajan <naveentherocker1234@gmail.com>
2017-12-27 00:13:34 +0530

    level

--- js/levels.js
+++ js/levels.js
@@ -3,7 +3,7 @@ function createTestLevel()
   return new Level([
     [door.none, door.right, door.right, door.right, door.right, door.none],
     [door.none, door.top, door.both, door.both, door.both, door.top],
-    [door.none, door.top, PlayerStartsAt(door.both), door.both, door.both, door.top],
+    [door.none, door.top, PlayerStartsAt(door.black), door.both, door.both, door.top],
     [door.none, door.top, door.both, door.both, door.both, door.top],
     [door.none, door.top, door.both, door.both, door.both, door.top],
     [door.none, door.top, door.both, door.both, door.both, door.top],
