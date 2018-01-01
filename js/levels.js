22850ba0c1e68974b98206187175895f2ca5a44f
STEdirisuriya <supunedirisuriya2002@gmail.com>
2018-01-01 19:34:12 +0530

    Add new tile and wall

--- js/levels.js
+++ js/levels.js
@@ -12,7 +12,7 @@ function createTestLevel()
 {
   return new Level([
     [door.none, door.right, door.right, door.right, door.goal, door.none],
-    [door.none, door.top, door.both, door.chessMate, door.both, door.chessStale],
+    [door.none, door.top, door.outdoor, door.chessMate, door.both, door.chessStale],
     [door.none, door.top, PlayerStartsAt(door.black), door.both, door.both, door.top],
     [door.none, door.top, door.both, door.both, door.both, door.top],
     [door.none, door.top, door.both, door.both, door.both, door.top],
