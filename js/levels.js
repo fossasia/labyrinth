19b7817eaf27da2343817b8bb80cfbadabc1d9f8
Yash Kumar Verma <yk.verma2000@gmail.com>
2018-01-06 01:15:03 +0530

    2

--- js/levels.js
+++ js/levels.js
@@ -11,7 +11,7 @@
 function createTestLevel()
 {
   return new Level([
-    [door.none, door.right, door.minecraft, door.none, door.goal, door.none],
+    [door.none, door.both, door.minecraft, door.none, door.goal, door.none],
     [door.none, door.river, door.outdoor, door.chessMate, door.both, door.chessStale],
     [door.none, door.top, PlayerStartsAt(door.black), door.green, door.banner, door.top],
     [door.none, door.top, door.boat, door.wheel, door.plain, door.top],
