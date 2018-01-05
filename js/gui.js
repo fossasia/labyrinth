88643b61620fffcd8b7e5a16d516c5443e1ce032
STEdirisuriya <supunedirisuriya2002@gmail.com>
2018-01-05 19:46:30 +0530

    Indent files

--- js/gui.js
+++ js/gui.js
@@ -1,4 +1,3 @@
-
 // defined in /js/player.js
 /* global newPlayer */
 
@@ -8,77 +7,76 @@
 var player;
 var level;
 
-window.addEventListener("load", function()
-{
-  var container = document.getElementById("tiles");
-  level = createTestLevel();
-  level.showIn(container);
-  player = newPlayer();
-  level.addPlayer(player);
+window.addEventListener("load", function () {
+    var container = document.getElementById("tiles");
+    level = createTestLevel();
+    level.showIn(container);
+    player = newPlayer();
+    level.addPlayer(player);
 });
 
 var keyHandlers = {
-  "ArrowLeft" : function() {
-    if (player.canMoveLeft()) {
-      player.moveLeft();
-    }
-    player.logPosition();
-  },
-  "ArrowRight" : function() {
-    if (player.canMoveRight()) {
-      player.moveRight();
-    }
-    player.logPosition();
-  },
-  "ArrowUp" : function() {
-    if (player.canMoveUp()) {
-      player.moveUp();
-    }
-    player.logPosition();
-  },
-  "ArrowDown" : function() {
-    if (player.canMoveDown()) {
-      player.moveDown();
-    }
-    player.logPosition();
-  },
-  "a" : function() {
-    level.visit();
-  },
+    "ArrowLeft": function () {
+        if (player.canMoveLeft()) {
+            player.moveLeft();
+        }
+        player.logPosition();
+    },
+    "ArrowRight": function () {
+        if (player.canMoveRight()) {
+            player.moveRight();
+        }
+        player.logPosition();
+    },
+    "ArrowUp": function () {
+        if (player.canMoveUp()) {
+            player.moveUp();
+        }
+        player.logPosition();
+    },
+    "ArrowDown": function () {
+        if (player.canMoveDown()) {
+            player.moveDown();
+        }
+        player.logPosition();
+    },
+    "a": function () {
+        level.visit();
+    },
 };
 
 // On Screen Keys
-$("#leftKey").on("click",function(){
-  if(player.canMoveLeft()){
-      player.moveLeft();
+$("#leftKey").on("click", function () {
+    if (player.canMoveLeft()) {
+        player.moveLeft();
     }
     player.logPosition();
 });
-$("#rightKey").on("click",function(){
-  if(player.canMoveRight()){
-    player.moveRight();
-  }
-  player.logPosition();
-});
-$("#upKey").on("click",function(){
-   if(player.canMoveUp()){
-      player.moveUp();
+$("#rightKey").on("click", function () {
+    if (player.canMoveRight()) {
+        player.moveRight();
     }
     player.logPosition();
 });
-$("#downKey").on("click",function(){
-   if(player.canMoveDown()){
-      player.moveDown();
+$("#upKey").on("click", function () {
+    if (player.canMoveUp()) {
+        player.moveUp();
+    }
+    player.logPosition();
+});
+$("#downKey").on("click", function () {
+    if (player.canMoveDown()) {
+        player.moveDown();
     }
     player.logPosition();
 });
 
-window.addEventListener("keydown", function(event) {
-  var handler = keyHandlers[event.key];
-  if (handler) {
-    handler();
-    console.log("handled:", handler);
-  } else {
-    console.log(event);
-  }
+window.addEventListener("keydown", function (event) {
+    var handler = keyHandlers[event.key];
+    if (handler) {
+        handler();
+        console.log("handled:", handler);
+    } else {
+        console.log(event);
+    }
 });
--- js/inventory.js
+++ js/inventory.js
@@ -5,80 +5,75 @@ by
 http://github.com/yashkumarverma/
 */
 
-function inventory()
-{
-	// Number of Items
-	this.items = [];
-	this.inventoryId = 0;
-	this.itemCollision = [];
+function inventory() {
+    // Number of Items
+    this.items = [];
+    this.inventoryId = 0;
+    this.itemCollision = [];
 
-	// Link Images
-	var imageDirectory = "images/game/inventory/";
+    // Link Images
+    var imageDirectory = "images/game/inventory/";
 
-	// add item to inventory
-	// Pass array comprising of 2 values. First name, second image
-	// player.add(['key','key.jpg'])
-	this.add = function(object)
-	{
-		/* OBJECT Details
-		0 has inventory id
-		1 has item name
-		2 has item image */
-		var itemStr = {
-			id:this.inventoryId++,
-			name:object[0],
-			image:object[1]			
-		};
+    // add item to inventory
+    // Pass array comprising of 2 values. First name, second image
+    // player.add(['key','key.jpg'])
+    this.add = function (object) {
+        /* OBJECT Details
+        0 has inventory id
+        1 has item name
+        2 has item image */
+        var itemStr = {
+            id: this.inventoryId++,
+            name: object[0],
+            image: object[1]
+        };
 
-		// Allow only one item of one name
-		// Collision
-		if(collisionCheck(itemStr,this.itemCollision))
-		{
-			this.items.push(itemStr);
-			this.itemCollision.push(itemStr.name);
-			addItem(itemStr);
-		}
-	};
+        // Allow only one item of one name
+        // Collision
+        if (collisionCheck(itemStr, this.itemCollision)) {
+            this.items.push(itemStr);
+            this.itemCollision.push(itemStr.name);
+            addItem(itemStr);
+        }
+    };
 
-	// check if item exist in inventory
-	this.has  = function(object){
-	    for (var i = this.items.length - 1; i >= 0; i--) {
-	      if(this.items[i].name === object){
-	        return true;
-	      }
-	      else{
-	        return false;
-	      }
-	    }
-	};
+    // check if item exist in inventory
+    this.has = function (object) {
+        for (var i = this.items.length - 1; i >= 0; i--) {
+            if (this.items[i].name === object) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
 
-	// to remove / consume item
-	this.remove = function(itemName){
-		var id = 0;
-		for (var i = this.items.length - 1; i >= 0; i--) {
-			if(this.items[i].name == itemName){
-				$("#inventoryItem_"+this.items[i].id).remove();
-			}
-		}
-	}
+    // to remove / consume item
+    this.remove = function (itemName) {
+        var id = 0;
+        for (var i = this.items.length - 1; i >= 0; i--) {
+            if (this.items[i].name == itemName) {
+                $("#inventoryItem_" + this.items[i].id).remove();
+            }
+        }
+    }
 
     // printInventoryItem
-    function addItem(obj){
-    	var data =	"<li id='inventoryItem_"+obj.id+"' class='collection-item avatar'>"+
-					"<img src='"+imageDirectory+obj.image+"' alt='"+obj.name+"' class='circle'>"+
-					"<b><span class='title'>"+obj.name+"</span></b>"+
-                	"</li>";
+    function addItem(obj) {
+        var data = "<li id='inventoryItem_" + obj.id + "' class='collection-item avatar'>" +
+            "<img src='" + imageDirectory + obj.image + "' alt='" + obj.name + "' class='circle'>" +
+            "<b><span class='title'>" + obj.name + "</span></b>" +
+            "</li>";
         $("#inventoryDisplay").append(data);
     }
 
-	function collisionCheck(itemName,dataset){
-  		if(dataset.length == 0)
-  			return true;
-		
-		if($.inArray(itemName, dataset) == -1)
-  			return true;
-  		else
-  			return false;
+    function collisionCheck(itemName, dataset) {
+        if (dataset.length == 0)
+            return true;
+
+        if ($.inArray(itemName, dataset) == -1)
+            return true;
+        else
+            return false;
     }
 }
-
