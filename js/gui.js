f41e9e5779269a8b21c2e18d11ec0cf35f49fe16
Yash Kumar Verma <yk.verma2000@gmail.com>
2018-01-07 00:53:33 +0530

    gui.js formatted

--- js/gui.js
+++ js/gui.js
@@ -1,102 +1,98 @@
-
 // defined in /js/player.js
 /* global newPlayer */
-
 // defined in /js/levels.js
 /* global createTestLevel */
-
 var player;
 var level;
 
-window.addEventListener("load", function()
-{
-  var container = document.getElementById("tiles");
-  level = createTestLevel();
-  level.showIn(container);
-  
-  var inputOptionsPromise = new Promise(function (resolve) {
-    // input your character here in the form, "src_url": "character_name",
-    resolve({
-      "characters/robo.svg": "Robo",
-      "characters/swammy.svg": "Swammy",
-      "characters/santa.svg": "Santa Clause",
-    });
-  });
-  player = newPlayer();
-  level.addPlayer(player);
+window.addEventListener("load", function() {
+    var container = document.getElementById("tiles");
+    level = createTestLevel();
+    level.showIn(container);
 
-  swal({
-    input: 'select',
-    inputOptions: inputOptionsPromise,
-    /* This is a way to get data quickly from swal */
-    inputValidator: function (value) {
-      player.changeCharacter(value);
-    }
-  });
+    var inputOptionsPromise = new Promise(function(resolve) {
+        // input your character here in the form, "src_url": "character_name",
+        resolve({
+            "characters/robo.svg": "Robo",
+            "characters/swammy.svg": "Swammy",
+            "characters/santa.svg": "Santa Clause",
+        });
+    });
+    player = newPlayer();
+    level.addPlayer(player);
+
+    swal({
+        input: 'select',
+        inputOptions: inputOptionsPromise,
+        /* This is a way to get data quickly from swal */
+        inputValidator: function(value) {
+            player.changeCharacter(value);
+        }
+    });
 });
 
 var keyHandlers = {
-  "ArrowLeft" : function() {
-    if (player.canMoveLeft()) {
-      player.moveLeft();
-    }
-    player.logPosition();
-  },
-  "ArrowRight" : function() {
-    if (player.canMoveRight()) {
-      player.moveRight();
-    }
-    player.logPosition();
-  },
-  "ArrowUp" : function() {
-    if (player.canMoveUp()) {
-      player.moveUp();
-    }
-    player.logPosition();
-  },
-  "ArrowDown" : function() {
-    if (player.canMoveDown()) {
-      player.moveDown();
-    }
-    player.logPosition();
-  },
-  "a" : function() {
-    level.visit();
-  },
+    "ArrowLeft": function() {
+        if (player.canMoveLeft()) {
+            player.moveLeft();
+        }
+        player.logPosition();
+    },
+    "ArrowRight": function() {
+        if (player.canMoveRight()) {
+            player.moveRight();
+        }
+        player.logPosition();
+    },
+    "ArrowUp": function() {
+        if (player.canMoveUp()) {
+            player.moveUp();
+        }
+        player.logPosition();
+    },
+    "ArrowDown": function() {
+        if (player.canMoveDown()) {
+            player.moveDown();
+        }
+        player.logPosition();
+    },
+    "a": function() {
+        level.visit();
+    },
 };
 
 // On Screen Keys
-$("#leftKey").on("click",function(){
-  if(player.canMoveLeft()){
-      player.moveLeft();
+$("#leftKey").on("click", function() {
+    if (player.canMoveLeft()) {
+        player.moveLeft();
     }
     player.logPosition();
 });
-$("#rightKey").on("click",function(){
-  if(player.canMoveRight()){
-    player.moveRight();
-  }
-  player.logPosition();
-});
-$("#upKey").on("click",function(){
-   if(player.canMoveUp()){
-      player.moveUp();
+$("#rightKey").on("click", function() {
+    if (player.canMoveRight()) {
+        player.moveRight();
     }
     player.logPosition();
 });
-$("#downKey").on("click",function(){
-   if(player.canMoveDown()){
-      player.moveDown();
+$("#upKey").on("click", function() {
+    if (player.canMoveUp()) {
+        player.moveUp();
+    }
+    player.logPosition();
+});
+$("#downKey").on("click", function() {
+    if (player.canMoveDown()) {
+        player.moveDown();
     }
     player.logPosition();
 });
 
 window.addEventListener("keydown", function(event) {
-  var handler = keyHandlers[event.key];
-  if (handler) {
-    handler();
-    console.log("handled:", handler);
-  } else {
-    console.log(event);
-  }
+    var handler = keyHandlers[event.key];
+    if (handler) {
+        handler();
+        console.log("handled:", handler);
+    } else {
+        console.log(event);
+    }
 });
