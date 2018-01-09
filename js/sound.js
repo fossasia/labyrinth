1598755786e9724260f85b89f6887534d1685651
Aditya Dutt <duttaditya18@gmail.com>
2018-01-09 15:01:21 +0530

    Fixed the Background music API with legal notice.

--- js/sound.js
+++ js/sound.js
@@ -1,11 +1,27 @@
-const backgroundAudio = ["audio/music/epic1.mp3", "audio/music/relaxing1.mp3"];
+const backgroundAudio = [
+    {
+        filename : "audio/music/bensound.com/epic1.mp3",
+        legalNotice : "Music by bensound.com",
+        link : "http://bensound.com/",
+    },
+    {
+        filename : "audio/music/bensound.com/relaxing1.mp3",
+        legalNotice : "Music by bensound.com",
+        link : "http://bensound.com/",
+    },
+];
 var backgroundNum = Math.floor(Math.random() * backgroundAudio.length);
-var backgroundPlay = new Audio(backgroundAudio[backgroundNum]);
+var backgroundPlay = new Audio(backgroundAudio[backgroundNum].filename);
 var audioPlay = null;
 $( document ).ready(function() {
     startBackgroundAudio();
 });
+function updateLegalBackground() {
+    $("#legalNotice").html(backgroundAudio[backgroundNum].legalNotice);
+    $("#legalNotice").attr("href", backgroundAudio[backgroundNum].link)
+}
 function startBackgroundAudio() {
+    updateLegalBackground();
     console.log(backgroundPlay);
     backgroundPlay.play();
 }
@@ -14,7 +30,8 @@ function nextBackgroundAudio() {
     if (backgroundNum !== backgroundAudio.length - 1) {
         backgroundNum = backgroundNum + 1
     }
-    backgroundPlay = new Audio(backgroundAudio[backgroundNum]);
+    updateLegalBackground();
+    backgroundPlay = new Audio(backgroundAudio[backgroundNum].filename);
     console.log(backgroundPlay);
     backgroundPlay.play();
 }
@@ -23,7 +40,8 @@ function previousBackgroundAudio() {
     if (backgroundNum !== 0) {
         backgroundNum = backgroundNum - 1
     }
-    backgroundPlay = new Audio(backgroundAudio[backgroundNum]);
+    updateLegalBackground();
+    backgroundPlay = new Audio(backgroundAudio[backgroundNum].filename);
     console.log(backgroundPlay);
     backgroundPlay.play();
 }
