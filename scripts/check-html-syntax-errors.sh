441d5843890b48f1d6f821e9e70d7d76afe0ae16
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-01-11 15:15:16 +0100

    change return code in case of failure

--- scripts/check-html-syntax-errors.sh
+++ scripts/check-html-syntax-errors.sh
@@ -16,7 +16,16 @@ cd "`dirname \"$0\"`"
       continue
     fi
     echo "Checking $file"
-    curl -H "Content-Type: text/html; charset=utf-8" --data-binary "@$file" 'https://validator.w3.org/nu/?out=gnu' 2>/dev/null | grep -I "error"
-    echo 
+    log="/tmp/error.log"
+    curl -H "Content-Type: text/html; charset=utf-8" --data-binary "@$file" 'https://validator.w3.org/nu/?out=gnu' 2>/dev/null | tee "$log" | grep --color=auto -I "error"
+    if cat "$log" | grep -qI "error"
+    then
+      allFine="false"
+    fi
+    echo
   done
+
+  if [ "$allFine" != "true" ]; then
+    exit 1
+  fi
 )
