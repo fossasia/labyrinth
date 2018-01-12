ce603f7df3714bed9fc67dc6c46211f66186cc3a
Beta-King <33036913+Beta-King@users.noreply.github.com>
2018-01-12 20:16:10 +0530

    Update sound.js

--- js/sound.js
+++ js/sound.js
@@ -62,6 +62,12 @@ const backgroundAudio = [
         legalNotice: "Music by Arjun Pandey",
         link: "http://github.com/arjunpandey2003",
     },
+        {
+        filename: "audio/music/bensound.com/idea.mp3",
+        backgroundSongName: "Idea",
+        author: "bensound.com",
+        legalNotice: "Music by bensound.com",
+    },
 
 ];
 var backgroundNum = Math.floor(Math.random() * backgroundAudio.length);
