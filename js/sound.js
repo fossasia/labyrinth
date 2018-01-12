66752e3a217d6dbc46aae1967e5503926d5b5662
Aditya Dutt <duttaditya18@gmail.com>
2018-01-12 19:18:50 +0530

    Made the numbering 1 indexed.

--- js/sound.js
+++ js/sound.js
@@ -137,5 +137,5 @@ function playAudio(audio, licenseNum) {
     audioPlay = new Audio("audio/" + audio);
     console.log(audioPlay);
     audioPlay.play();
-    $("#tileLicense").html(creativeCommonsLicense[licenseNum]);
+    $("#tileLicense").html(creativeCommonsLicense[licenseNum - 1]);
 }
