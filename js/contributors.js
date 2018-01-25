f75e44e4e3d57e3f52de3caff61563a4cbf420d6
PRP <prp9812@gmail.com>
2018-01-26 00:45:34 +0530

    added back to top feature for howtoplay

--- js/contributors.js
+++ js/contributors.js
@@ -47,17 +47,3 @@ $(window).resize(function () {
     var viewportWidth = $(window).width();
     $('#monitor').html(viewportWidth);
 });
-
-//Add a back to top feature
-window.onscroll = function () {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("scrollToTop").style.display = "block";
-    } else {
-        document.getElementById("scrollToTop").style.display = "none";
-    }
-};
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-} 
