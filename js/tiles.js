374d211a0a15b6ba322857a880ce68efd1c69cb9
Hubert Zimny <hmzimny@gmail.com>
2018-10-31 21:36:07 +0100

    Update tiles.js

--- js/tiles.js
+++ js/tiles.js
@@ -320,7 +320,7 @@ const door = {
 	
 	sea: Object.assign({}, OpenDoors, {
 		createImages: function(){
-			this.wallTop = this.createImage("tiles/rooms/wall/seasidewall.svg");
+			this.wallTop = this.createImage("tiles/rooms/door/seasidewalltop.svg");
             this.wallRight = this.createImage("tiles/rooms/door/seasidedoor.svg");
             this.ground = this.createImage("tiles/rooms/floor/caro.svg");
 		},
