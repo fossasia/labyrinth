0e6cdcb4b9da5f8254eacf4ea8a17bd785694171
Shizerq <maciek22414@interia.pl>
2018-11-02 21:06:38 +0100

    Update js/levels.js

--- js/levels.js
+++ js/levels.js
@@ -19,8 +19,6 @@ function createFirstLevel()
     [door.fairysWheel, door.design, door.cricketGround, door.treasure, door.yellow, door.red],
     [door.both, door.top, door.marina, door.treasureKey, door.drawn, door.night],
     [door.both, door.boat, door.river, desert.universe, door.cake, door.newYear],
-    [door.mountains, door.boat, door.river, desert.universe, door.cake, door.newYear],
-
 
 
     
