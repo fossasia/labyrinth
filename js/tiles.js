abe7dcd6edc8c2374e722b02c86278f4f2a6a2db
akshit15013 <akshit15013@users.noreply.github.com>
2018-11-21 09:12:12 +0530

    Add a Hand Drawn Tile

--- js/tiles.js
+++ js/tiles.js
@@ -594,6 +594,13 @@ const door = {
             this.ground = this.createImage("tiles/rooms/floor/self_made.svg");
         },
     }),
+    drawnbyhand: Object.assign({}, OpenDoors, {
+        createImages: function() {
+            this.wallTop = this.createImage("tiles/rooms/door/top.svg");
+            this.wallRight = this.createImage("tiles/rooms/door/right.svg");
+            this.ground = this.createImage("tiles/rooms/floor/drawnbyhand.svg");
+        },
+    }),
     fabricate: Object.assign({}, OpenDoors, {
         createImages: function() {
             this.wallTop = this.createImage("tiles/rooms/door/top.svg");
