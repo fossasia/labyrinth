56870d14a6c0d7259b734d6b35ee248b8976249b
AirWolfXD <2521andy@gmail.com>
2018-11-22 15:48:05 -0800

    Update tiles.js

--- js/tiles.js
+++ js/tiles.js
@@ -400,7 +400,8 @@ const door = {
             this.wallTop = this.createImage("tiles/rooms/door/redDoor.svg");
             this.wallRight = this.createImage("tiles/rooms/wall/bloodsidewall2.svg");
             this.ground = this.createImage("tiles/rooms/floor/blood.svg");
-        } visit: function() {
+        },
+            visit: function() {
             alertInfo("Be careful, Many people died in this tsunami");
             playAudio("spook.mp3");
             this.wallTop.show();
