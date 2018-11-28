e3a36a0170a492ef0d8a63154067c348e169c73b
Nishith P <nishithp11@gmail.com>
2018-11-28 21:59:16 +0530

    Update gui.js

--- js/gui.js
+++ js/gui.js
@@ -68,7 +68,7 @@ function choosePlayerCharacter(player) {
             "characters/telebishop.svg": "Telebishop",
             "characters/LittleAlien.png": "Little Alien",
             "characters/barbariansword.svg": "Barbarian Sword",
-            "characters/Twitter.svg": "Twitter",
+            "characters/mario.svg": "Super Mario",
         });
     });
     swal({
