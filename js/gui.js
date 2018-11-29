8cfc83fc2c2a4bec20160b2d222c386dec68736a
Rahul Metre <34718002+RahulMetre03@users.noreply.github.com>
2018-11-29 19:14:29 +0530

    Update gui.js

--- js/gui.js
+++ js/gui.js
@@ -69,7 +69,7 @@ function choosePlayerCharacter(player) {
             "characters/telebishop.svg": "Telebishop",
             "characters/LittleAlien.png": "Little Alien",
             "characters/barbariansword.svg": "Barbarian Sword",
-            "characters/mario.png": "Mario
+            "characters/mario.png": "Mario",
             "characters/rabbit.svg": "Rabbit",
         });
     });
