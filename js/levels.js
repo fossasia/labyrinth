88979c5c076e94c4b3f92e972cecaeb5f7551de5
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2018-12-04 23:12:14 +0530

    Fixed typo

--- js/levels.js
+++ js/levels.js
@@ -22,7 +22,7 @@ function createFirstLevel()
     [door.dark1, door.minecraftEntry, door.minecraft, door.orange, door.rollingball, door.reddish, door.winter1, door.goal, door.none],
     [door.newlines, door.orchestra, door.Forbidden, door.chocolate, door.guitarCase, door.winter2],
 
-    [door.sea, door.sofa, door.outdoor, door.chessMate, door.texture, door.chessStale door.programmer],
+    [door.sea, door.sofa, door.outdoor, door.chessMate, door.texture, door.chessStale, door.programmer],
     [door.drawnbyme, Superhero.art, PlayerStartsAt(door.black), door.green, door.banner, door.threeHeads],
     [door.fairysWheel, door.design, door.cricketGround, door.treasure, door.yellow, door.red],
     [door.both, door.blood, door.marina, door.treasureKey, door.drawn, door.night],
