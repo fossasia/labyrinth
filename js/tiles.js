bec1138b7bb2ffb2d4a24270798db8af5dd92885
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2018-12-09 22:24:59 +0530

    Added code tile
    * Added hand drawn tile
    
    * Added tile
    
    * Update tiles.js
    
    * Added my tile to the game

--- js/tiles.js
+++ js/tiles.js
@@ -399,6 +399,24 @@ const door = {
             this.ground = this.createImage("tiles/rooms/floor/handarjun.svg");
         },
     }),
+    
+    handDrawnCode: Object.assign({}, OpenDoors, {
+        canEnterFromTheRight() { return true; },
+        canLeaveToTheRight() { return true; },
+        canEnterFromTheTop() { return true; },
+        canLeaveToTheTop() { return true; },
+        canEnterFromTheBottom() { return true; },
+        canLeaveToTheBottom() { return true; },
+        canEnterFromTheLeft() { return true; },
+        canLeaveToTheLeft() { return true; },
+        
+        createImages: function() {
+            this.wallTop = this.createImage("tiles/rooms/door/top.svg"); /* Alter these attributes to specify a custom wall tile for the floor tile.  Do not forget to implement the movements with canEnter/LeaveFromTheRight, ... */
+            this.wallRight = this.createImage("tiles/rooms/door/right.svg");
+            this.ground = this.createImage("tiles/rooms/floor/handDrawnCode.svg");
+        },
+    }),
+    
     starywall: Object.assign({}, OpenDoors, {
     canLeaveToTheTop: function(player) {
             return false;
