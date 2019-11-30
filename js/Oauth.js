7fb5cfc55cf21092b9c778ce449511f773b23688
shubham <chd.nonu26@gmail.com>
2019-11-30 11:40:21 +0530

    oauth SignIn feature implemented
    oauth signIn implemented

---
+++ js/Oauth.js
@@ -0,0 +1,23 @@
+function onSignIn()
+ { console.log('hey')
+    const googleUser = gapi.auth2.getAuthInstance().currentUser.get();
+    var profile=googleUser.getBasicProfile();
+    $('.g-signin2').css('display','none');
+    $('.username').css('display','block');
+    $('.username').text(profile.getEmail());
+    $('.profile-pic').css('display','block');
+    $('.profile-pic').attr('src',profile.getImageUrl());
+    $('.signout').css('display','block');
+}
+function signout(){
+    var auth2=gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function(){
+        swal('You Have been successfully signed out');
+        $('.g-signin2').css('display','block');
+    $('.username').css('display','none');
+    $('.username').text('none');
+    $('.profile-pic').css('display','none');
+    $('.profile-pic').attr('src','none');
+    $('.signout').css('display','none');
+    })
+}
